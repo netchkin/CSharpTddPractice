e4031e559974a05185f71c16055b34ad060f56c1
netchkin <pavel.janecka@gmail.com>
2015-10-02 22:24:19 +0200

    initial commit, first test for simple possibility

---
+++ CodeEvalDoubleTroubleCSharpTDD/DoubleTrouble.Tests/VariantsDetectorTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace DoubleTrouble.Tests
+{
+    public class VariantsDetectorTest
+    {
+        [Fact]
+        public void DetectVariants_ValidCodeWithoutStars_ReturnsOnePossibility()
+        {
+            var sut = new VariantsDetector();
+            int variants = sut.DetectVariants("AAAAAA");
+            Assert.Equal(1, variants);
+        }
+    }
+
+    internal class VariantsDetector
+    {
+        public VariantsDetector()
+        {
+        }
+    }
+}
