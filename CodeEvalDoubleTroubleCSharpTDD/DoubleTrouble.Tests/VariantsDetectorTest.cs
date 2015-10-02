db6d24dbb2dd583b48487685ad0c4997fb49110f
netchkin <pavel.janecka@gmail.com>
2015-10-02 22:52:43 +0200

    the variants detector has been moved to its own project and the variants calculation was made private because of the previous tests amendments

--- CodeEvalDoubleTroubleCSharpTDD/DoubleTrouble.Tests/VariantsDetectorTest.cs
+++ CodeEvalDoubleTroubleCSharpTDD/DoubleTrouble.Tests/VariantsDetectorTest.cs
@@ -61,46 +61,4 @@ namespace DoubleTrouble.Tests
         }
 
     }
-
-    internal class VariantsDetector
-    {
-        public VariantsDetector()
-        {
-        }
-
-        public int DetectVariants(string ancientCode)
-        {
-            int variants = 0;
-
-            var ancientCodePartLength = ancientCode.Length / 2;
-            string ancientCodePartOne = ancientCode.Substring(0, ancientCodePartLength);
-            string ancientCodePartTwo = ancientCode.Substring(ancientCodePartLength);
-
-            variants = ComputeVariantsForCodeParts(ancientCodePartOne, ancientCodePartTwo);
-
-            return variants;
-        }
-
-        internal int ComputeVariantsForCodeParts(string codePartOne, string codePartTwo)
-        {
-            int variants = 1;
-
-            for (int i = 0; i < codePartOne.Length; i++)
-            {
-                char currentInPartOne = codePartOne[i];
-                char currentInPartTwo = codePartTwo[i];
-                if (currentInPartOne != '*' && currentInPartTwo != '*' && currentInPartOne != currentInPartTwo)
-                {
-                    variants = 0;
-                    break;
-                }
-                else if (currentInPartOne == '*' && currentInPartTwo == '*')
-                {
-                    variants *= 2;
-                }
-            }
-
-            return variants;
-        }
-    }
 }
---
+++ CodeEvalDoubleTroubleCSharpTDD/DoubleTrouble/VariantsDetector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleTrouble
+{
+    public class VariantsDetector
+    {
+        public VariantsDetector()
+        {
+        }
+
+        public int DetectVariants(string ancientCode)
+        {
+            int variants = 0;
+
+            var ancientCodePartLength = ancientCode.Length / 2;
+            string ancientCodePartOne = ancientCode.Substring(0, ancientCodePartLength);
+            string ancientCodePartTwo = ancientCode.Substring(ancientCodePartLength);
+
+            variants = ComputeVariantsForCodeParts(ancientCodePartOne, ancientCodePartTwo);
+
+            return variants;
+        }
+
+        private int ComputeVariantsForCodeParts(string codePartOne, string codePartTwo)
+        {
+            int variants = 1;
+
+            for (int i = 0; i < codePartOne.Length; i++)
+            {
+                char currentInPartOne = codePartOne[i];
+                char currentInPartTwo = codePartTwo[i];
+                if (currentInPartOne != '*' && currentInPartTwo != '*' && currentInPartOne != currentInPartTwo)
+                {
+                    variants = 0;
+                    break;
+                }
+                else if (currentInPartOne == '*' && currentInPartTwo == '*')
+                {
+                    variants *= 2;
+                }
+            }
+
+            return variants;
+        }
+    }
+}
